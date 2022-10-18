9045a8a1dd3b5931be591b80e0ec26daad44f7f0
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2022-10-18 11:07:21 +0200

    [FIX: fix attempt to access backoffice

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOSTED_HOST'),
   port: env.int('HOSTED_PORT'),
+  url: 'https://yohark.de/jazzhands-backoffice',
   app: {
     keys: env.array('APP_KEYS'),
   },
