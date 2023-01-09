00d5fa32f28e76a602f6923428808b35c09cb03e
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2023-01-10 00:52:25 +0100

    [FIX]: went back to hosted env variables

--- config/server.js
+++ config/server.js
@@ -1,7 +1,15 @@
+// module.exports = ({ env }) => ({
+//   host: env('LOCAL_HOST'),
+//   port: env.int('LOCAL_PORT'),
+// //   url: 'https://yohark.de/jazzhands-backoffice',
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
 module.exports = ({ env }) => ({
-  host: env('LOCAL_HOST'),
-  port: env.int('LOCAL_PORT'),
-//   url: 'https://yohark.de/jazzhands-backoffice',
+  host: env('HOSTER_HOST'),
+  port: env.int('HOSTER_PORT'),
+  url: 'https://yohark.de/jazzhands-backoffice',
   app: {
     keys: env.array('APP_KEYS'),
   },
