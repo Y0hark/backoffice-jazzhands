8a14a1e261b0f559dae565c3d4abb4a6cf016daf
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2023-03-14 03:13:48 +0100

    [FIX]: wrong response population

--- src/api/guide/controllers/guide.js
+++ src/api/guide/controllers/guide.js
@@ -5,12 +5,28 @@ module.exports = createCoreController('api::guide.guide', ({ strapi }) => ({
 	async findOne(ctx) {
 		const { slug } = ctx.params;
 
-		const entity = await strapi.db.query('api::guide.guide').findOne(
-			{ where: { slug } }
-		);
-		const sanitizedEntity = await this.sanitizeOutput(entity);
+		const guide = await strapi.db.query('api::guide.guide').findOne({ where: { slug } });
+
+		console.log(guide.id);
+
+		const entity = await strapi.entityService.findOne('api::guide.guide', guide.id, {
+			populate: {
+				'content': {
+					populate: '*',
+				},
+				'author': '*',
+				'category': '*',	
+			},
+		})
+
+		if (!entity) {
+			return ctx.notFound('Guide not found');
+		}
+
+		const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
 		return this.transformResponse(sanitizedEntity);
 	},
 
 }));
+
